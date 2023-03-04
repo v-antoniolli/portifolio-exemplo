c2dd422d50379852996b2f8467f75de5870fc678
v-antoniolli <125698945+v-antoniolli@users.noreply.github.com>
2023-03-04 16:54:08 -0300

    onClickScrollTo('id')

---
+++ javaScript/index.js
@@ -0,0 +1,28 @@
+/*
+function onClickHabilidades() {
+  document.getElementById("habilidades").scrollIntoView();
+}
+function onClickPortifolio() {
+  document.getElementById("portifolio").scrollIntoView();
+}
+function onClickExperiencias() {
+  document.getElementById("experiencias").scrollIntoView();
+}*/
+
+
+
+/*
+
+UMA MANEIRA MAIS FACIL DE FAZER FUNÇÕES PARA O COMANDO DE CIMA É:
+IR NA ANCORA (LINK) E ANEXAR .... 
+onclick="onClickScrollTo('nome do id pra onde quer ir')"
+A PARTE DO ID É COM ASPAS SIMPLES!!!!
+E NA PARTE DO SCRIPT
+ESCREVER A FUNÇÃO QUE VAI EXECUTAR ISSO
+function onClickScrollTo(id){
+  document.getElementById(id).scrollIntoView();
+}
+*/
+function onClickScrollTo(id) {
+  document.getElementById(id).scrollIntoView();
+}
